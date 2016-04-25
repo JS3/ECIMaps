003bb86741e05965559774ce044e338d33a700a3
Diana María del Pilar Socha Díaz <dianamaria696@gmail.com>
2016-04-25 17:10:51 -0500

    Cambios en la documentacion menores

--- src/main/java/eci/edu/arsw/controllers/EcimapController.java
+++ src/main/java/eci/edu/arsw/controllers/EcimapController.java
@@ -8,3 +8,40 @@ package eci.edu.arsw.controllers;
 public class EcimapController {
     
 }
+
+//
+//@RestController
+//@RequestMapping("/blueprints")
+//public class BlueprintController {
+//    
+//    @Autowired
+//    ServicesFacade services;
+//    
+//    
+//    @RequestMapping(value="/check",method = RequestMethod.GET)        
+//    public String check() {
+//        return "REST API OK";        
+//    }
+//    
+//    @RequestMapping(method = RequestMethod.POST)        
+//    public ResponseEntity<?> addBlueprint(@RequestBody Blueprint p) {       
+//        services.addNewBlueprint(p.getName(),p);
+//        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+//    }
+//
+//    
+//    @RequestMapping(method = RequestMethod.GET)
+//    public Set<String> allBlueprintNames() {       
+//        return services.getBlueprintNames();
+//    }
+//    
+//    @RequestMapping(value = "/{blueprintname}",method = RequestMethod.GET)        
+//    public Blueprint getABlueprint(@PathVariable("blueprintname") String blueprintname) {       
+//        return services.getBlueprintByName(blueprintname);
+//    }
+//
+//    
+//    
+//    
+//    
+//}
